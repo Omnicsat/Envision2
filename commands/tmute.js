33ee26148aad0d319bd59ffb7847e1f98343d984
retomnine <retomnine@gmail.com>
2021-11-21 11:33:16 -0800

    more fixes

--- commands/tmute.js
+++ commands/tmute.js
@@ -14,7 +14,7 @@ module.exports = {
 				.setRequired(true))
 		.setDefaultPermission(false),
 	async execute(interaction) {
-		await interaction.deferReply();
+		interaction.deferReply();
 		const user = interaction.options.getMentionable('user');
 		const timeout = interaction.options.getNumber('timeout');
 		const channel = interaction.channel;
